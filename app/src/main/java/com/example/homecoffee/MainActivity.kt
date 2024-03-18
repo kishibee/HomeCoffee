b01fe00bc5c2ccbe495ece5eaad44818ba10c090
kishibee <raflyade9@gmail.com>
2024-03-18 15:42:02 +0700

    first commit

---
+++ app/src/main/java/com/example/homecoffee/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.homecoffee
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.homecoffee.ui.theme.HomeCoffeeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HomeCoffeeTheme {
+                HomeCoffeeApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeCoffeeApp() {
+
+}
+
+@Preview(showBackground = true, device = Devices.PIXEL_4)
+@Composable
+fun HomeCofeeAppPreview() {
+    HomeCoffeeTheme {
+        HomeCoffeeApp()
+    }
+}
