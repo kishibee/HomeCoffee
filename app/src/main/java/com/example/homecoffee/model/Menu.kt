7d4d14f0327969d6502fb4a85dfc509ddaf4544a
kishibee <raflyade9@gmail.com>
2024-03-22 09:16:09 +0700

    add material 3 dependency

---
+++ app/src/main/java/com/example/homecoffee/model/Menu.kt
@@ -0,0 +1,1 @@
+package com.example.homecoffee.model
---
+++ app/src/main/java/com/example/homecoffee/ui/components/Search.kt
@@ -0,0 +1,10 @@
+package com.example.homecoffee.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.google.android.material.search.SearchBar
+
+@Composable
+fun Search(modifier: Modifier = Modifier) {
+    SearchBar
+}
